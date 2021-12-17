9ee4c251dc8e5aaeac9bed975bcf8a45419ad212
bassam <bassam.zahra@outlook.com>
2021-12-17 12:52:39 +0100

    first model configuration

---
+++ CommuteTypeTracker/CommuteTypeTracker/Model/CommuteTypeTrackerContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CommuteTypeTracker.Model
+{
+    public class CommuteTypeTrackerContext : DbContext
+    {
+        protected CommuteTypeTrackerContext(DbContextOptions<CommuteTypeTrackerContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<DailyCommute> DailyCommute { get; set; }
+
+    }
+}
---
+++ CommuteTypeTracker/CommuteTypeTracker/Model/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommuteTypeTracker.Model
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(64)]
+        public string FirstName { get; set; }
+        [Required]
+        [MaxLength(64)]
+        public string LastName { get; set; }
+        public DateTime CreatedAt { get; }
+        public DateTime UpdatedAt { get; }
+
+        [MaxLength(64)]
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress] 
+        public string Email { get; set; }
+        public CommuteType DefaultCommuteType { get; set; }
+        public List<DailyCommute> dailyCommutes { get; set; }
+    }
+}
